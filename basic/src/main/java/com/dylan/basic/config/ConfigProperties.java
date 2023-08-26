8586d8c9f845c251a1a41eef2f8af972789bf42c
Dylan <dongweilaidong@gmail.com>
2023-08-26 22:09:50 +0800

    update springboot-basic

---
+++ basic/src/main/java/com/dylan/basic/config/ConfigProperties.java
@@ -0,0 +1,19 @@
+package com.dylan.basic.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author Dylan
+ * @Date 2023/8/26
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "base.config")
+public class ConfigProperties {
+    private String name;
+    private String port;
+    private String applicationName;
+    private String active;
+}
---
+++ basic/src/main/java/com/dylan/basic/controller/ConfigPropertiesController.java
@@ -0,0 +1,28 @@
+package com.dylan.basic.controller;
+
+import com.dylan.basic.config.ConfigProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author Dylan
+ * @Date 2023/8/26
+ */
+
+@RestController
+@RequestMapping("/config")
+public class ConfigPropertiesController {
+
+    @Autowired
+    ConfigProperties configProperties;
+
+    @GetMapping("/")
+    public void test(){
+        System.out.println("base.config.applicationName: " + configProperties.getApplicationName());
+        System.out.println("base.config.name: " + configProperties.getName());
+        System.out.println("base.config.port: " + configProperties.getPort());
+        System.out.println("base.config.active: " + configProperties.getActive());
+    }
+}
